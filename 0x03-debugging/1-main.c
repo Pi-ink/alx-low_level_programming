1e7f9c94e0ba38426711ff8b1868de89579fabc8
Pi-ink <boudalilara1@gmail.com>
2023-03-23 10:59:27 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,20 +5,19 @@
  * Return: 0
  */
 
+
+
 int main(void)
 
 {
 	int i;
 
 	printf("infinite loop incoming :(\n");
-
 	i = 0;
-
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
 	printf("infinite loop avoided! \\o/\n");
-
 	return (0);
 }
